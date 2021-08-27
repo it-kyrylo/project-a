1936f573cb4e413b230c2444bca4ba7d39e97fef
MiroslavDionisiev <73421736+MiroslavDionisiev@users.noreply.github.com>
2021-08-27 10:58:03 +0300

    Created player classes for performance and statistic data. Also created generic repository for players data.

---
+++ ProjectA/ProjectA/Models/StatisticsPlayerData.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ProjectA.Models
+{
+	public class StatisticsPlayerData : Player
+	{
+		[JsonProperty("minutes")]
+		public int MinutesInPlay { get; set; }
+
+		[JsonProperty("goals_scored")]
+		public int GoalsScored { get; set; }
+
+		[JsonProperty("assists")]
+		public int Assists { get; set; }
+
+		[JsonProperty("yellow_cards")]
+		public int YellowCards { get; set; }
+
+		[JsonProperty("red_cards")]
+		public int RedCards { get; set; }
+	}
+}
---
+++ ProjectA/ProjectA/Repositories/IPlayersRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using ProjectA.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectA.Repositories
+{
+    public interface IPlayersRepository<T> where T : class
+    {
+        public Task<PlayersRepository<T>> Create();
+
+        public T GetPlayerData(string playerName);
+
+        public int GetPlayerId(string playerName);
+
+        public IEnumerable<T> GetAllPlayers();
+    }
+}
