e6add1e52fd0284bacdbe72102871b7cc883c1c6
MiroslavDionisiev <73421736+MiroslavDionisiev@users.noreply.github.com>
2021-08-27 10:58:03 +0300

    Created player classes for performance and statistic data. Also created generic repository for players data.

---
+++ ProjectA/ProjectA/Models/PerformancePlayerData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace ProjectA.Models
+{
+    public class PerformancePlayerData : Player
+    {
+        [JsonProperty("form")]
+        public double Form { get; set; }
+        
+        [JsonProperty("points_per_game")]
+        public double PointsPerGame { get; set; }
+        
+        [JsonProperty("total_points")]
+        public int TotalPoints { get; set; }
+        
+        [JsonProperty("influence_rank_type")]
+        public int Influence { get; set; }
+        
+        [JsonProperty("creativity_rank_type")]
+        public int Creativity { get; set; }
+        
+        [JsonProperty("threat_rank_type")]
+        public int Threat { get; set; }
+        
+        [JsonProperty("ict_index_rank_type")]
+        public int IndexRank { get; set; }
+
+        [JsonProperty("now_cost")]
+        public int Price { get; set; }
+    }
+}
---
+++ ProjectA/ProjectA/Repositories/PlayersRepository.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using ProjectA.Models;
+using ProjectA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class PlayersRepository<T> : IPlayersRepository<T> where T : class
+{
+	private readonly Dictionary<string, T> playersData;
+
+    public async Task<PlayersRepository<T>> Create()
+    {
+        PlayersRepository<T> currentClass = new PlayersRepository<T>();
+        List<T> data = await currentClass.LoadPlayers();
+        currentClass.PopulatePlayersData(data);
+        return currentClass;
+    }
+
+    private PlayersRepository()
+	{
+        this.playersData = new Dictionary<string, T>();
+    }
+
+    private async Task<List<T>> LoadPlayers()
+    {
+        const string URL = "https://fantasy.premierleague.com/api/bootstrap-static/";
+
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(URL);
+
+            HttpResponseMessage response = await client.GetAsync(URL);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<List<T>>(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    private void PopulatePlayersData(List<T> data)
+    {
+        foreach (T currentData in data)
+        {
+            string key;
+            if (currentData.GetType() == typeof(PerformancePlayerData))
+            {
+                PerformancePlayerData tmp = (PerformancePlayerData)(object)currentData;
+                key = tmp.FirstName + " " + tmp.LastName;
+            }
+            else
+            {
+                StatisticsPlayerData tmp = (StatisticsPlayerData)(object)currentData;
+                key = tmp.FirstName + " " + tmp.LastName;
+            }
+            this.playersData[key] = currentData;
+        }
+    }
+
+    public T GetPlayerData(string playerName)
+    {
+        if(this.playersData.ContainsKey(playerName) == true)
+        {
+            return this.playersData[playerName];
+        }
+        return null;
+    }
+
+    public int GetPlayerId(string playerName)
+    {
+        if (this.playersData.ContainsKey(playerName) == true)
+        {
+            int id;
+            T currentPlayer = this.playersData[playerName];
+            if (currentPlayer.GetType() == typeof(PerformancePlayerData))
+            {
+                PerformancePlayerData tmp = (PerformancePlayerData)(object)currentPlayer;
+                id = tmp.Id;
+            }
+            else
+            {
+                StatisticsPlayerData tmp = (StatisticsPlayerData)(object)currentPlayer;
+                id = tmp.Id;
+            }
+            return id;
+        }
+        return -1;
+    }
+
+    public IEnumerable<T> GetAllPlayers()
+    {
+        return this.playersData.Values;
+    }
+}
