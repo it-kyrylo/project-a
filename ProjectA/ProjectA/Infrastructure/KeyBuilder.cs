864bb37a4664cef12969d2bf2908870d5e9c7763
MiroslavDionisiev <miro9122@gmail.com>
2021-08-30 14:48:33 +0300

    Code refactored

---
+++ ProjectA/ProjectA/Infrastructure/KeyBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectA.Infrastructure
+{
+    public static class KeyBuilder
+    {
+        public static string Build(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+    }
+}
---
+++ ProjectA/ProjectA/Repositories/PlayersRepository/IPlayersRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectA.Models.PlayersModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectA.Repositories.PlayersRepository
+{
+    public interface IPlayersRepository
+    {
+        public Task<Player> GetPlayerDataAsync(string playerName);
+
+        public Task<IEnumerable<Player>> GetAllPlayersAsync();
+    }
+}
